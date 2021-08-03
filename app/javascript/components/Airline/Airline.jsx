af7b08e095eaa4aba9bdce9efc82de46fe07149b
Vishwas-75 <vishwashnvishu75@gmail.com>
2021-08-04 02:11:35 +0530

    added  airline show component

--- app/javascript/components/Airline/Airline.jsx
+++ app/javascript/components/Airline/Airline.jsx
@@ -1,10 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Modal } from 'react-bootstrap'
+import { Rowcontainer, Colcontainer, Container, Box, Title, Button, Card } from '../common/styles/Common.styled'
+import { ImageContainer, Header } from './styles/Airline.styled'
+import { Subtilte } from '../Airlines/styles/Airlines.styled'
+import Input from '../common/component/Input'
 
-function Airline() {
+function Airline(props) {
+    const [airline, setAirline] = useState({})
+    const [reviews, setReviews] = useState([])
+    const [review, setReview] = useState({ title: '', description: '', score: '' })
+    const [open, setOpen] = useState(false)
+    useEffect(() => {
+        const slug = props.match.params.slug
+        const url = `/api/v1/airlines/${slug}`
+
+        axios.get(url).
+            then((res) => {
+                const result = res.data.data.attributes
+                const resultReview = res.data.included || []
+                const extractedReview = resultReview.map((review) => review.attributes)
+                setAirline({ ...result })
+                setReviews([...extractedReview])
+            }
+            ).
+            catch((res) => console.log(res.err))
+
+    }, [])
+
+    const handleChange = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        setReview({ ...review, name: value })
+
+    }
     return (
-        <div>
-            Airline Show
-        </div>
+        <React.Fragment>
+            <Colcontainer>
+                <Header >
+                    <ImageContainer>
+                        <img src={airline.image_url} width='100px' height='100px' />
+                    </ImageContainer>
+                </Header>
+                <Rowcontainer style={{ marginTop: '3.5rem' }} jContent='space-around'>
+                    <Box><Title size={'15px'}>{airline.name}</Title></Box>
+                    <div style={{ flexGrow: 0.6 }} />
+                    <Box><Button onClick={() => setOpen(true)}> Add Review </Button></Box>
+
+                </Rowcontainer>
+                <Rowcontainer aItems='center' jContent='center'>
+                    <Colcontainer style={{ width: '70%' }}>
+                        {
+                            reviews?.map((review) => <Card mB='10px'>
+                                <Container direction='column'>
+                                    <Title>{review.title}</Title>
+                                    <Subtilte>{review.description}</Subtilte>
+                                    <Subtilte>{review.score}</Subtilte>
+                                </Container>
+                            </Card>)
+                        }
+                    </Colcontainer>
+                </Rowcontainer>
+
+            </Colcontainer>
+            <Modal show={open} onHide={() => setOpen(false)}>
+                <Input
+                    name='title'
+                    label='Title'
+                    onChange={handleChange}
+                    value={review.title}
+                    type='text'
+                />
+                <Input
+                    name='description'
+                    label='Description'
+                    onChange={handleChange}
+                    value={review.description}
+                    type='text'
+                />
+            </Modal>
+        </React.Fragment>
     )
 }
 
---
+++ app/javascript/components/Airline/styles/Airline.styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ImageContainer = styled.div`
+position: relative;
+top:50px;
+left:80px;
+background-color: #ffffff;
+border-radius: 10px;
+box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.1);
+img{
+    border-radius: 8px;
+}
+`
+export const Header = styled.div`
+background-color: black;
+height: 100px; 
+width: 100%;
+display: flex;
+`
