dc4873f8318bfa026de15173c376373d09d9f9b6
Vishwas-75 <vishwashnvishu75@gamil.com>
2021-09-30 01:41:15 +0530

    add new fuctionality

--- app/javascript/components/Airline/Airline.jsx
+++ app/javascript/components/Airline/Airline.jsx
@@ -1,86 +1,87 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { Modal } from 'react-bootstrap'
-import { Rowcontainer, Colcontainer, Container, Box, Title, Button, Card } from '../common/styles/Common.styled'
-import { ImageContainer, Header } from './styles/Airline.styled'
-import { Subtilte } from '../Airlines/styles/Airlines.styled'
-import Input from '../common/component/Input'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Modal } from "react-bootstrap";
+
+import {
+  Rowcontainer,
+  Colcontainer,
+  Box,
+  Title,
+  Button,
+} from "../common/styles/Common.styled";
+import { ImageContainer, Header } from "./styles/Airline.styled";
+import { makeRequest } from "../common/apicall/RequestAssist";
+import Review from "../common/component/Review";
+import ReviewCard from "../common/component/ReviewCard";
 
 function Airline(props) {
-    const [airline, setAirline] = useState({})
-    const [reviews, setReviews] = useState([])
-    const [review, setReview] = useState({ title: '', description: '', score: '' })
-    const [open, setOpen] = useState(false)
-    useEffect(() => {
-        const slug = props.match.params.slug
-        const url = `/api/v1/airlines/${slug}`
+  const [airline, setAirline] = useState({});
+  const [reviews, setReviews] = useState([]);
 
-        axios.get(url).
-            then((res) => {
-                const result = res.data.data.attributes
-                const resultReview = res.data.included || []
-                const extractedReview = resultReview.map((review) => review.attributes)
-                setAirline({ ...result })
-                setReviews([...extractedReview])
-            }
-            ).
-            catch((res) => console.log(res.err))
+  const [open, setOpen] = useState(false);
+  const fetchAirline = () => {
+    const slug = props.match.params.slug;
+    const url = `/api/v1/airlines/${slug}`;
 
-    }, [])
+    axios
+      .get(url)
+      .then((res) => {
+        const result = res.data.data.attributes;
+        const resultReview = res.data.included || [];
+        const extractedReview = resultReview.map((review) => review.attributes);
+        setAirline({ ...result });
+        setReviews([...extractedReview]);
+      })
+      .catch((res) => console.log(res.err));
+  };
 
-    const handleChange = (e) => {
-        const name = e.target.name
-        const value = e.target.value
-        setReview({ ...review, name: value })
+  useEffect(() => {
+    fetchAirline();
+  }, []);
 
-    }
-    return (
-        <React.Fragment>
-            <Colcontainer>
-                <Header >
-                    <ImageContainer>
-                        <img src={airline.image_url} width='100px' height='100px' />
-                    </ImageContainer>
-                </Header>
-                <Rowcontainer style={{ marginTop: '3.5rem' }} jContent='space-around'>
-                    <Box><Title size={'15px'}>{airline.name}</Title></Box>
-                    <div style={{ flexGrow: 0.6 }} />
-                    <Box><Button onClick={() => setOpen(true)}> Add Review </Button></Box>
+  const handleSubmit = async (review) => {
+    let url = `/api/v1/airlines/${airline.id}/airlinereviews`;
 
-                </Rowcontainer>
-                <Rowcontainer aItems='center' jContent='center'>
-                    <Colcontainer style={{ width: '70%' }}>
-                        {
-                            reviews?.map((review) => <Card mB='10px'>
-                                <Container direction='column'>
-                                    <Title>{review.title}</Title>
-                                    <Subtilte>{review.description}</Subtilte>
-                                    <Subtilte>{review.score}</Subtilte>
-                                </Container>
-                            </Card>)
-                        }
-                    </Colcontainer>
-                </Rowcontainer>
+    const formData = new FormData();
+    formData.append("airlinereview[title]", review.title);
+    formData.append("airlinereview[description]", review.description);
+    formData.append("airlinereview[score]", review.score / 10);
+    formData.append("airlinereview[airline_id]", airline.id);
+    const response = await makeRequest(url, "post", formData);
+    setOpen(!open);
+    fetchAirline();
+  };
 
-            </Colcontainer>
-            <Modal show={open} onHide={() => setOpen(false)}>
-                <Input
-                    name='title'
-                    label='Title'
-                    onChange={handleChange}
-                    value={review.title}
-                    type='text'
-                />
-                <Input
-                    name='description'
-                    label='Description'
-                    onChange={handleChange}
-                    value={review.description}
-                    type='text'
-                />
-            </Modal>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <Colcontainer>
+        <Header>
+          <ImageContainer>
+            <img src={airline.image_url} width="100px" height="100px" />
+          </ImageContainer>
+        </Header>
+        <Rowcontainer style={{ marginTop: "3.5rem" }} jContent="space-around">
+          <Box>
+            <Title size={"15px"}>{airline.name}</Title>
+          </Box>
+          <div style={{ flexGrow: 0.6 }} />
+          <Box>
+            <Button onClick={() => setOpen(true)}> Add Review </Button>
+          </Box>
+        </Rowcontainer>
+        <Rowcontainer aItems="center" jContent="center">
+          <ReviewCard
+            reviews={reviews}
+            setReviews={setReviews}
+            airlineId={airline.id}
+          />
+        </Rowcontainer>
+      </Colcontainer>
+      <Modal show={open} onHide={() => setOpen(!open)}>
+        <Review onSubmit={handleSubmit} />
+      </Modal>
+    </React.Fragment>
+  );
 }
 
-export default Airline
+export default Airline;
--- app/javascript/components/common/component/Input.jsx
+++ app/javascript/components/common/component/Input.jsx
@@ -1,36 +1,63 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Label, Container } from '../styles/Common.styled'
+import React, { useState, useImperativeHandle } from "react";
+import styled from "styled-components";
+import { isEmpty } from "../../../util/utilFunction";
+import { Label, Container, W3Text } from "../styles/Common.styled";
 
 const InputField = styled.input`
-border: none;
-outline: none;
-background-color: #ffff;
-color: #2e36ac;
-`
+  border: none;
+  outline: none;
+  background-color: #ffff;
+  color: #2e36ac;
+`;
+const Error = styled(W3Text)`
+  color: red;
+  font-size: 12px;
+`;
 
+function Input({ name, type, placeholder, onChange, className, value, label },ref) {
+  const [error, setError] = useState({});
 
+  const handleBlur = () => {
+    if (isEmpty(value)) {
+      setError({ ...error, message: `${label} is required` });
+    }
+  };
 
-function Input(props) {
-  const { name, type, placeholder, onChange, className, value, error, children, label } = props
+  const handleInput = () => {
+    setError({});
+  };
+
+  useImperativeHandle(ref, () => ({
+    handleSubmit() {
+      if (isEmpty(value)) {
+        setError({ ...error, message: `${label} is required` });
+      }
+    },
+  }));
+
   return (
     <React.Fragment>
-      <Container direction='column'>
-        <Label htmlFor={name}>{label}</Label>
+      <Container direction="column">
+        <Label htmlFor={name} size="16px">
+          {label}
+        </Label>
         <InputField
+          onInput={handleInput}
           id={name}
           name={name}
           type={type}
           placeholder={placeholder}
           onChange={onChange}
+          onBlur={handleBlur}
           value={value}
           className={className}
-          style={error && { border: 'solid 1px red' }}
+          ref={ref}
+          style={error.message && { border: "solid 1px red" }}
         />
-        {error && <p>{error}</p>}
+        {!!error && <Error>{error.message}</Error>}
       </Container>
     </React.Fragment>
-  )
+  );
 }
 
-export default Input
+export default React.forwardRef(Input);
